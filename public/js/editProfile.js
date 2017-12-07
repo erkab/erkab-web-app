5d57707a7f6969ad8c12d78288f092bf8760418e
HeshamSaleh <hsaleh88@gmail.com>
2017-12-07 04:09:12 +0200

    - Added 'Change Password' button & Section. - Added some profile animations.

--- public/js/editProfile.js
+++ public/js/editProfile.js
@@ -1,42 +1,66 @@
 $(document).ready(function () {
     $("#save-cancel-btn-group").hide();
-    $(".profileInput").prop("readonly", true);
-    $(".profileInput").addClass("readonly-input");
+    $(".profileInput").prop("readonly", true).addClass("readonly-input");
     $("#passwordField").hide();
+    $("#changePass-btn-group").hide();
     $("#nameField").hide();
 
     $("#editBtn").click(function () {
-        $("html, body").animate({scrollTop: $("#profileH3").offset().top - 86}, "slow");
-        $(".profileInput").prop("readonly", false);
         $("#editBtn").hide();
         $("#save-cancel-btn-group").show();
         $("#profileH3").html("<i class=\"fa fa-pencil-square-o\"></i> Edit Profile");
         $(".profileInput").removeClass("readonly-input");
-        $("#passwordField").show();
         $("#nameField").show();
         $("#fullNameField").hide();
-    })
+        $("#changePassLink").hide();
+    });
 
     $("#saveBtn").click(function () {
-        $(".profileInput").prop("readonly", true);
+        $(".profileInput").prop("readonly", true).addClass("readonly-input");
         $("#save-cancel-btn-group").hide();
         $("#editBtn").show();
         $("#profileH3").html("<i class=\"fa fa-user-circle-o\"></i> Profile");
-        $(".profileInput").addClass("readonly-input");
         $("#passwordField").hide();
         $("#nameField").hide();
         $("#fullNameField").show();
-    })
+        $("#changePassLink").show();
+    });
 
     $("#cancelBtn").click(function () {
         $("html, body").animate({scrollTop: $("#profileH3").offset().top - 86}, "slow");
-        $(".profileInput").prop("readonly", true);
+        $(".profileInput").prop("readonly", true).addClass("readonly-input");
         $("#save-cancel-btn-group").hide();
         $("#editBtn").show();
         $("#profileH3").html("<i class=\"fa fa-user-circle-o\"></i> Profile");
-        $(".profileInput").addClass("readonly-input");
         $("#passwordField").hide();
         $("#nameField").hide();
         $("#fullNameField").show();
+        $("#changePassLink").show();
+    });
+
+    $("#changePassLink").click(function () {
+        $("#mainForm").hide(function () {
+            $("html, body").animate({scrollTop: $("#profileH3").offset().top - 86}, "slow");
+        });
+        $("#profileH3").html("<i class=\"fa fa-lock\"></i> Change Password");
+        $("#passwordField").show();
+        $("#changePass-btn-group").show();
+    });
+
+    $("#confirmPassBtn").click(function () {
+        $("html, body").animate({scrollTop: $("#profileH3").offset().top - 86}, "slow");
+        $("#profileH3").html("<i class=\"fa fa-user-circle-o\"></i> Profile");
+        $("#mainForm").show();
+        $("#passwordField").hide();
+        $("#changePass-btn-group").hide();
+    });
+
+    $("#cancelPassBtn").click(function () {
+        $("html, body").animate({scrollTop: $("#profileH3").offset().top - 86}, "slow");
+        $("#profileH3").html("<i class=\"fa fa-user-circle-o\"></i> Profile");
+        $("#mainForm").show();
+        $("#passwordField").hide();
+        $("#changePass-btn-group").hide();
     })
+
 })
